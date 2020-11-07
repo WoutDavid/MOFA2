a28cd3685ad6b6d82f541a3bd087293f336e5c6d
David <wouters.david@hotmail.com>
2020-11-07 18:18:33 +0100

    now contains working code to add the barcode.metrics a metadata. A lot of debugging code is still there just in case.

--- create_seurat_adapted.R
+++ create_seurat_adapted.R
@@ -9,6 +9,7 @@ outfile <- "/media/david/Puzzles/IBP/human/cellranger/seurat_human.RDS"
 ###############
 ## Load data ##
 ###############
+rm(counts)
 counts <- Read10X_h5("/media/david/Puzzles/IBP/human/cellranger/human_filtered_feature_bc_matrix.h5")
 #counts <- Read10X(paste0(basedir,"/e18_mouse_brain_fresh_5k_raw_feature_bc_matrix.h5"))
 
@@ -21,12 +22,23 @@ seurat <- CreateSeuratObject(
   project = "scRNA+scATAC_human",
   min.cells = 1
 )
+seurat
+seurat@assays$RNA@counts
+rownames(seurat@assays$RNA@counts)
+colnames(seurat@assays$RNA@counts)
+dim(seurat@assays$RNA@counts)
 
 # Add ATAC modality
 seurat[["ATAC"]] <- CreateAssayObject(counts = counts["Peaks"][[1]])
+dim(seurat@assays$ATAC@counts)
+##86394 features, 3332 cells
+rownames(seurat@assays$ATAC@counts)
+colnames(seurat@assays$ATAC@counts)
 
+##all gene names are in caps
+##all cells are in caps with regex: CAAGACAAGGACCTTG-1
 seurat
-
+#rows are cells
 ##################
 ## Add metadata ##
 ##################
@@ -34,13 +46,23 @@ seurat
 rm(metadata)
 test <-fread("/media/david/Puzzles/IBP/human/cellranger/human_per_barcode_metrics.csv")
 dim(test)
+colnames(test)
+rownames(test)
+head(test)
+
 #read in the metadata, replace the barcordes by barcodes without the -1 in there
+#I decided not to replace the -1. It might be less pretty and interpretable, but it makes the merging
+#of the metadata a lot easier
 metadata <- fread("/media/david/Puzzles/IBP/human/cellranger/human_per_barcode_metrics.csv") %>%
- .[,barcode:=gsub("-1","",barcode)]
+ .[,barcode:=gsub("-1","-1",barcode)]
 dim(metadata)
+colnames(metadata)
+rownames(metadata)
+head(metadata)
 #filter those rows that do not have a 1 in their is_cell column
 metadata <- metadata[metadata$is_cell==1]
 dim(metadata)
+
 #this is code that only works if you have this specific metadata, it's not necessary.
 # dt <- data.table(barcode=colnames(seurat)) %>%
 #   merge(metadata,by="barcode", all.x=TRUE) %>%
@@ -48,14 +70,19 @@ dim(metadata)
 #   .[!is.na(celltype),c("pass_rnaQC","pass_accQC"):=TRUE] %>%
 #   tibble::column_to_rownames("barcode")
 
-
-seurat <- AddMetaData(seurat, metadata)
-?AddMetaData
+colnames(metadata)
+rownames(metadata)
+head(metadata[,1])
+head(seurat@meta.data)
+dt <- data.table(barcode=colnames(seurat)) %>%
+  merge(metadata,by="barcode", all.x=TRUE) %>%
+  tibble::column_to_rownames("barcode")
+dt
+seurat <- AddMetaData(seurat, dt)
 head(seurat@meta.data)
 dim(seurat@meta.data)
-sum(is.na(seurat@meta.data$is_cell))
+
 ##########
 ## Save ##
 ##########
-#note, you skipped a big part of the metadata cause the celltype thing didn't work 
 saveRDS(seurat, outfile, compress = FALSE)
